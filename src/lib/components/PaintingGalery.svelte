ce8550aaaa4c85610bf7424843204ecf0511d103
Abdallah HAMMAD <abdallah.hammad@epitech.eu>
2026-01-28 18:52:06 +0800

    Add PaintingGallery component to display a grid of paintings

---
+++ src/lib/components/PaintingGalery.svelte
@@ -0,0 +1,11 @@
+<script lang="ts"> 
+    import PaintingCard from '$lib/components/PaintingCard.svelte';
+    import { paintingsStore } from '$lib/paintingsStore.svelte';
+
+</script>
+
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 p-6">
+    {#each Object.keys(paintingsStore) as paintingId (paintingId)}
+        <PaintingCard {paintingId} />
+    {/each}
+</div>
